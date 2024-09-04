d3688295c4a2cc4a1c43d7365c49bc3542ee984b
Zababurin Olexii <852gin@gmail.com>
2024-09-04 12:33:12 +0300

    refactoring social links. wip

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { XIcon } from "./components/icons/XIcon";
 import { LinkedInIcon } from "./components/icons/LinkedInIcon";
 import { FacebookIcon } from "./components/icons/FacebookIcon";
 import GitHubButton from "react-github-btn";
+import {Icon} from "@/app/components/Icon";
 
 const Home = () => {
   return (
@@ -40,62 +41,30 @@ const Home = () => {
 
         {/* height 4rem */}
         <div className="bg-black/80 h-[4rem] flex items-center absolute w-full">
-          <footer className="mx-auto w-full max-w-7xl px-4 md:px-6 lg:px-8 flex items-center justify-center gap-4 md:text-xl font-inter text-[#8a8a8e]">
-            <span className="text-base text-[#4e4e52]">share it</span>
+          <footer
+              className="mx-auto w-full max-w-7xl px-4 md:px-6 lg:px-8 flex items-center justify-center gap-4 md:text-xl font-inter text-[#8a8a8e]">
+            <span className="text-base text-[#4e4e52]">Hire me</span>
             <a
-              href="#"
-              onClick={(e) => {
-                window.open(
-                  "https://twitter.com/intent/tweet?text=%F0%9F%94%A5%F0%9F%8E%89%20Check%20out%20this%20awesome%20%23AI%20demo%20by%20%40Deepgram%20and%20%40lukeocodes%0A%0A%20https%3A//aura-tts-demo.deepgram.com",
-                  "",
-                  "_blank, width=600, height=500, resizable=yes, scrollbars=yes"
-                );
-
-                return e.preventDefault();
-              }}
-              aria-label="share on twitter"
-              target="_blank"
+                href="https://www.upwork.com/freelancers/~01db8123e8a706c7e5"
+                aria-label="hire me on Upwork"
+                target="_blank"
             >
-              <XIcon className="mb-1" />
+              <Icon name="upworkIcon" opacity={0.66} className="mb-1"/>
             </a>
             <a
-              href="#"
-              onClick={(e) => {
-                window.open(
-                  "https://www.linkedin.com/shareArticle?mini=true&url=https%3A//aura-tts-demo.deepgram.com&title=Excellent review on my website reviews",
-                  "",
-                  "_blank, width=600, height=500, resizable=yes, scrollbars=yes"
-                );
-
-                return e.preventDefault();
-              }}
-              aria-label="share on Linkedin"
+                href="https://www.linkedin.com/in/alexei-zababaurin-209479110/"
+                aria-label="open my Linkedin"
+                target="_blank"
             >
-              <LinkedInIcon className="mb-1" />
+              <Icon name="linkedInIcon" fill="rgb(108, 138, 158)" className="mb-1"/>
             </a>
+            <span className="text-base text-[#4e4e52]">contact me</span>
             <a
-              href="#"
-              onClick={(e) => {
-                window.open(
-                  "https://www.facebook.com/sharer/sharer.php?u=https%3A//aura-tts-demo.deepgram.com",
-                  "",
-                  "_blank, width=600, height=500, resizable=yes, scrollbars=yes"
-                );
-
-                return e.preventDefault();
-              }}
-              target="_blank"
-              aria-label="share on Facebook"
+                href="mailto:alexei.zababurin@gmail.com"
+                aria-label="email me"
+                target="_blank"
             >
-              <FacebookIcon className="mb-1" />
-            </a>
-            <div className="border-l border-[#4e4e52] w-px h-7">&nbsp;</div>
-            <a
-              className="text-base font-semibold"
-              href="https://deepgram.com/contact-us"
-              target="_blank"
-            >
-              contact us
+              <Icon name="email-icon" fill="rgb(138, 138, 142)"  className="mb-1"/>
             </a>
           </footer>
         </div>
